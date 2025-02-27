a7df09a97da159db4797d3df236b22ce38fbef13
Biswadeep-Paul <paulbiswadeep27@gmail.com>
2025-02-28 00:03:06 +0530

    ok

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -14,7 +14,7 @@ userRouter.post("/book-appointment", authUser, bookAppointment)
 userRouter.get("/appointments", authUser, listAppointment)
 userRouter.post("/cancel-appointment", authUser, cancelAppointment)
 // userRouter.post("/payment-razorpay", authUser, paymentRazorpay)
- userRouter.post("/verifyRazorpay", authUser, verifyRazorpay)
+//  userRouter.post("/verifyRazorpay", authUser, verifyRazorpay)
 // userRouter.post("/payment-stripe", authUser, paymentStripe)
 // userRouter.post("/verifyStripe", authUser, verifyStripe)
 
