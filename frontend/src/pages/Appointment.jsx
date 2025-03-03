cba56da3feae149e624a22656aa65ab9a29968fe
amzonclone <adityj81@gmail.com>
2025-03-03 23:01:02 +0530

    doctor profile location added

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import  { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
