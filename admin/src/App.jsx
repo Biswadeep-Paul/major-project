3083eebfbc6250c2e170a399c56b07e89b9c1ac3
Biswadeep-Paul <paulbiswadeep27@gmail.com>
2025-05-06 21:38:20 +0530

    ok

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,7 +3,7 @@ import { DoctorContext } from './context/DoctorContext';
 import { AdminContext } from './context/AdminContext';
 import { Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Dashboard from './pages/Admin/Dashboard';
