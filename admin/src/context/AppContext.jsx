3c73d8b76f1b25e6683aea4cfc85e889e2b4f864
Biswadeep-Paul <paulbiswadeep27@gmail.com>
2025-05-26 23:27:08 +0530

    ok

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -1,4 +1,6 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 
 export const AppContext = createContext()
@@ -8,6 +10,8 @@ const AppContextProvider = (props) => {
     const currency = import.meta.env.VITE_CURRENCY
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
+     const [ratings, setRatings] = useState([]) 
+
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
     // Function to format the date eg. ( 20_01_2000 => 20 Jan 2000 )
@@ -15,6 +19,23 @@ const AppContextProvider = (props) => {
         const dateArray = slotDate.split('_')
         return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
     }
+
+    
+
+    const getRatingsData = async (doctorId) => {
+    try {
+        const { data } = await axios.get(`${backendUrl}/api/doctor/${doctorId}/ratings`) // <-- replace docId in URL
+        if (data.success) {
+            setRatings(data.ratings)
+            return { ratings: data.ratings, avgRating: data.avgRating }
+        } else {
+            toast.error(data.message)
+        }
+    } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+    }
+}
 
     // Function to calculate the age eg. ( 20_01_2000 => 24 )
     const calculateAge = (dob) => {
@@ -27,6 +48,7 @@ const AppContextProvider = (props) => {
         currency,
         slotDateFormat,
         calculateAge,
+        ratings,getRatingsData,
     }
 
     return (
